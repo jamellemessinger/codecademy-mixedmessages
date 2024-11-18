ae84491f0d3256f1b85ded2bba14e696b333b90f
jamellemessinger <jamswoof914@gmail.com>
2024-11-18 05:59:50 -0700

    load data from data folder. add new line to testSet, update package.json

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,1 +1,17 @@
-console.log("hello");
+// const data = require("../data/testSet.text");
+// console.log(data);
+
+import fs from 'fs/promises';
+import path from 'path';
+
+// Resolve the file path
+const filePath = path.resolve('../data/testSet.text');
+
+try {
+  // Read the file content
+  const text = await fs.readFile(filePath, 'utf8');
+  console.log(text);
+} catch (error) {
+  console.error('Error:', error); 
+}
+
